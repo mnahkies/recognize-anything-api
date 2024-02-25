6e6d251c8534046094706aea2bc11371f6f1ce0d
Michael Nahkies <michael@nahkies.co.nz>
2024-02-25 10:28:44 +0000

    fix: version tag

--- bin/docker-publish.sh
+++ bin/docker-publish.sh
@@ -4,5 +4,10 @@ set -exo pipefail
 
 ./bin/docker-build.sh
 
+TAG=$(python setup.py --version)
+
 docker tag recognize-anything-api mnahkies/recognize-anything-api:latest
+docker tag recognize-anything-api "mnahkies/recognize-anything-api:$TAG"
+
 docker push mnahkies/recognize-anything-api:latest
+docker push "mnahkies/recognize-anything-api:$TAG"
